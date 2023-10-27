6556d2d9f19d5741cff882e4fd60234769f9749c
exxsid <cortezleoanthony5@gmail.com>
2023-10-27 19:30:40 +0800

    add login with email and password authentication

--- src/utils/AuthenticationFirebase.js
+++ src/utils/AuthenticationFirebase.js
@@ -1,4 +1,8 @@
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 import { FIREBASE_APP } from "../../config/firebase_config";
 
@@ -18,4 +22,18 @@ isValidEmail = (email) => {
     return false;
   }
   return true;
+};
+
+export const logInWithEmailAndPassword = (email, password) => {
+  // verify the user's email
+  if (!isValidEmail("email.example.com")) {
+    throw "Wrong email format";
+  }
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      return userCredential;
+    })
+    .catch((error) => {
+      return error;
+    });
 };
